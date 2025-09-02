5b72bd229c4ff59358634572f6d053f8c8bf8214
Emilia <emilia.ferlin@sbsistemas.com.br>
2025-09-02 08:38:14 -0300

    refactor: ajustes no card do financeiro

--- lib/src/components/cards/card_financeiro.dart
+++ lib/src/components/cards/card_financeiro.dart
@@ -22,28 +22,53 @@ class CardFinanceiro extends StatelessWidget {
         padding: const EdgeInsets.all(12.0),
         child: Row(
           children: [
+            Checkbox(
+              value: dadosFinanceiro?.quitada == 0 ? false : true,
+              activeColor: primaryColor,
+              onChanged: onChangedCheckBox,
+            ),
+            SizedBox(width: 12.0),
             Expanded(
-              child: Row(
+              child: Column(
                 children: [
-                  Checkbox(
-                    value: dadosFinanceiro?.quitada == 0 ? false : true,
-                    activeColor: primaryColor,
-                    onChanged: onChangedCheckBox,
+                  Row(
+                    children: [
+                      Expanded(child: Text(dadosFinanceiro?.descricao ?? "")),
+                      IconButton(
+                        onPressed: () {
+                          acaoDespesaReceitaModalBottomSheet(
+                            context,
+                            dadosFinanceiro ?? FinanceiroModel(),
+                            db ?? Financeirodao(),
+                          );
+                        },
+                        icon: Icon(Icons.edit_note_outlined, size: 42.0),
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 12.0),
-                  Column(children: [Text(dadosFinanceiro?.descricao ?? "")]),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          dadosFinanceiro?.data ?? DateTime.now().toString(),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 12.0),
+                        child: Text(
+                          dadosFinanceiro?.valor ?? "0.0",
+                          style: TextStyle(
+                            color:
+                                dadosFinanceiro?.tipo == 0
+                                    ? Colors.red
+                                    : Colors.green,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ),
-            IconButton(
-              onPressed: () {
-                acaoDespesaReceitaModalBottomSheet(
-                  context,
-                  dadosFinanceiro ?? FinanceiroModel(),
-                  db ?? Financeirodao(),
-                );
-              },
-              icon: Icon(Icons.edit_note_outlined, size: 42.0),
             ),
           ],
         ),
