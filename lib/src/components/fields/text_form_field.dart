02e0633ccfa8e6171e7e8fd91643e91e37b0e58d
Emilia <emilia.ferlin@sbsistemas.com.br>
2025-09-02 08:37:58 -0300

    refactor: ajustes no formulario do financeiro

--- lib/src/components/fields/text_form_field.dart
+++ lib/src/components/fields/text_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lista_tarefa/src/utils/constantes.dart';
 
 class TextFormFieldComponente extends StatelessWidget {
@@ -7,6 +8,8 @@ class TextFormFieldComponente extends StatelessWidget {
   final String? hinText;
   final Icon? icone;
   final String? Function(String?)? validator;
+  final bool? enable;
+  final List<TextInputFormatter>? textInputFormatter;
   const TextFormFieldComponente({
     super.key,
     this.controller,
@@ -14,32 +17,38 @@ class TextFormFieldComponente extends StatelessWidget {
     this.icone,
     this.labelText,
     this.validator,
+    this.enable,
+    this.textInputFormatter,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      style: TextStyle(fontSize: 16.0),
-      cursorErrorColor: Colors.red,
-      cursorColor: primaryColor,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      decoration: InputDecoration(
-        icon: icone,
-        labelText: labelText,
-        hintText: hinText,
-        hintStyle: TextStyle(color: primaryColor),
-        labelStyle: TextStyle(color: Colors.black),
-        focusColor: primaryColor,
-        fillColor: primaryColor,
-        iconColor: Colors.black,
-        hoverColor: primaryColor,
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: primaryColor ?? Colors.black),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: TextFormField(
+        controller: controller,
+        style: TextStyle(fontSize: 16.0),
+        cursorErrorColor: Colors.red,
+        cursorColor: primaryColor,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        decoration: InputDecoration(
+          icon: icone,
+          labelText: labelText,
+          hintText: hinText,
+          hintStyle: TextStyle(color: primaryColor),
+          labelStyle: TextStyle(color: Colors.black),
+          focusColor: primaryColor,
+          fillColor: primaryColor,
+          iconColor: Colors.black,
+          hoverColor: primaryColor,
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColor ?? Colors.black),
+          ),
         ),
+        enabled: enable ?? true,
+        inputFormatters: textInputFormatter,
+        validator: validator,
       ),
-
-      validator: validator,
     );
   }
 }
--- lib/src/view/financeiro/formulario_financeiro_view.dart
+++ lib/src/view/financeiro/formulario_financeiro_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lista_tarefa/src/components/fields/text_form_field.dart';
 import 'package:lista_tarefa/src/components/navigation_bar/navigation_bar.dart';
 import 'package:lista_tarefa/src/database/financeiro/financeiroDao.dart';
 import 'package:lista_tarefa/src/model/financeiro/financeiro_model.dart';
 import 'package:lista_tarefa/src/utils/constantes.dart';
+import 'package:brasil_fields/brasil_fields.dart';
 
 class FormularioFinanceiroView extends StatefulWidget {
   final FinanceiroModel? financeiroModel;
@@ -22,8 +24,12 @@ class FormularioFinanceiroView extends StatefulWidget {
 class _FormularioFinanceiroViewState extends State<FormularioFinanceiroView> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController descricaoText = TextEditingController();
+  TextEditingController valorText = TextEditingController();
+  TextEditingController dataText = TextEditingController();
+  DateTime? dataInicial;
   Financeirodao db = Financeirodao();
   FinanceiroModel financAux = FinanceiroModel();
+  int? despesaReceita;
 
   @override
   void initState() {
@@ -31,6 +37,9 @@ class _FormularioFinanceiroViewState extends State<FormularioFinanceiroView> {
         widget.financeiroModel?.id != null &&
         widget.financeiroModel?.descricao != null) {
       descricaoText.text = widget.financeiroModel?.descricao ?? "";
+      valorText.text = widget.financeiroModel?.valor ?? "";
+      dataText.text = widget.financeiroModel?.data ?? "";
+      despesaReceita = widget.financeiroModel?.tipo;
       setState(() {});
     }
     super.initState();
@@ -73,6 +82,82 @@ class _FormularioFinanceiroViewState extends State<FormularioFinanceiroView> {
                     return null;
                   },
                 ),
+                TextFormFieldComponente(
+                  controller: valorText,
+                  icone: Icon(Icons.attach_money_outlined),
+                  labelText: "Valor",
+                  hinText: "Informe o valor",
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Campo obrigatório";
+                    }
+                    return null;
+                  },
+                  textInputFormatter: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    RealInputFormatter(moeda: true),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RadioListTile<int>(
+                        title: const Text('Despesa'),
+                        value: 0,
+                        activeColor: primaryColor,
+                        groupValue: despesaReceita,
+                        onChanged: (int? value) {
+                          setState(() {
+                            despesaReceita = value;
+                          });
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: RadioListTile<int>(
+                        title: const Text('Receita'),
+                        value: 1,
+                        groupValue: despesaReceita,
+                        activeColor: primaryColor,
+                        onChanged: (int? value) {
+                          setState(() {
+                            despesaReceita = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                  child: TextFormFieldComponente(
+                    controller: dataText,
+                    icone: Icon(Icons.calendar_month_outlined),
+                    labelText: "Data",
+                    hinText: "Informe a data",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Campo obrigatório";
+                      }
+                      return null;
+                    },
+                    enable: false,
+                  ),
+                  onTap: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: dataInicial ?? DateTime.now(),
+                      firstDate: DateTime(2021),
+                      lastDate: DateTime(2030),
+                    );
+
+                    if (pickedDate != null) {
+                      setState(() {
+                        dataInicial = pickedDate;
+                        dataText.text = UtilData.obterDataDDMMAAAA(pickedDate);
+                      });
+                    }
+                  },
+                ),
               ],
             ),
           ),
@@ -84,31 +169,37 @@ class _FormularioFinanceiroViewState extends State<FormularioFinanceiroView> {
           style: ElevatedButton.styleFrom(backgroundColor: primaryColor),
           onPressed: () async {
             if (_formKey.currentState?.validate() == true) {
-              if (widget.isNovaDespesaReceita == false &&
-                  widget.financeiroModel?.id != null &&
-                  widget.financeiroModel?.descricao != null) {
-                await updateDespesaReceita();
+              if (despesaReceita == null) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Selecione se é despesa ou receita")),
+                );
               } else {
-                await addDespesaReceita();
-              }
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return BottomNavigationBarApp(indexPage: 1);
-                  },
-                ),
-                (route) => false,
-              );
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    widget.isNovaDespesaReceita == true
-                        ? 'Despesa/Receita Adicionada!'
-                        : 'Despesa/Receita Editada!',
+                if (widget.isNovaDespesaReceita == false &&
+                    widget.financeiroModel?.id != null &&
+                    widget.financeiroModel?.descricao != null) {
+                  await updateDespesaReceita();
+                } else {
+                  await addDespesaReceita();
+                }
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return BottomNavigationBarApp(indexPage: 1);
+                    },
                   ),
-                ),
-              );
+                  (route) => false,
+                );
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      widget.isNovaDespesaReceita == true
+                          ? 'Despesa/Receita Adicionada!'
+                          : 'Despesa/Receita Editada!',
+                    ),
+                  ),
+                );
+              }
             }
           },
           label: Text(
@@ -126,9 +217,9 @@ class _FormularioFinanceiroViewState extends State<FormularioFinanceiroView> {
       id: widget.financeiroModel?.id,
       descricao: descricaoText.text,
       quitada: widget.financeiroModel?.quitada,
-      valor: widget.financeiroModel?.valor,
-      data: widget.financeiroModel?.data,
-      tipo: widget.financeiroModel?.tipo,
+      valor: valorText.text,
+      data: dataText.text,
+      tipo: despesaReceita,
     );
     db.update(financAux);
   }
@@ -138,9 +229,9 @@ class _FormularioFinanceiroViewState extends State<FormularioFinanceiroView> {
       id: 1,
       descricao: descricaoText.text,
       quitada: 0,
-      valor: "0.0",
-      data: "212121",
-      tipo: 0,
+      valor: valorText.text,
+      data: dataText.text,
+      tipo: despesaReceita,
     );
     db.add(financAux);
   }
